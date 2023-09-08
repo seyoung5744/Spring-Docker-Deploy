14f8ae681d716378f595470938cf3262c781c9ad
seyoung5744 <seyoung574458@gmail.com>
2023-09-08 22:31:16 +0900

    init project

---
+++ src/main/java/com/example/springdockerdeploy/SpringDockerDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerDeployApplication.class, args);
+	}
+
+}
